33daf9aa68781535d4fab0296a659386b5ebb5ce
Prakruti_Tops <109515514+prakrutitops@users.noreply.github.com>
2025-07-24 17:43:35 +0530

    commit

--- Module1/7-7/maxnumex.dart
+++ Module1/7-7/maxnumex.dart
@@ -3,17 +3,19 @@ import 'dart:io';
 void main()
 {
   print("Enter Any Number");
-  int num = int.parse(stdin.readLineSync().toString());
-  int max = 0;
+  int num = int.parse(stdin.readLineSync().toString());//5872
+  int max = 0;//2//7//8
+
   while(num>0)
   {
-      int rem = num%10;//4//3//2//1
+      int rem = num%10;//2//7//8//5
       if(rem>max)
       {
           max = rem;
       }
       num=num ~/ 10;
   }
+
   print("Biggest Digit is : $max");
 
 
